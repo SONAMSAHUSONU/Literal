b873bf755e429d3a2f5ab3c9bb86709b09413afa
sonam <sonamsahu199518@gmail.com>
2022-04-19 21:55:57 +0530

    Add project files.

---
+++ Literal/Program.cs
@@ -0,0 +1,153 @@
+﻿/*using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        bool b1 = true;
+        bool b2 = false;
+        Console.WriteLine(b1);// output on True
+        Console.WriteLine(b2);// False
+        Console.ReadLine();
+        }
+    }*/
+
+/* using System;
+
+ class Program
+ {
+     static void Main(string[] args)
+     {
+     bool b1 = 1; //error
+     bool b2 = abc; //error
+     Console.WriteLine(b1);
+     Console.WriteLine(b2);
+     Console.ReadLine();
+     }
+ }    
+ */
+
+/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool b1 = True;//error
+        bool b2 = False;//error
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+        Console.ReadLine();
+    }
+}
+*/
+/*using System;  
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte b = 77;
+        Console.WriteLine(b);
+        short s = 45;
+        Console.WriteLine(s);
+        int i = 123431;
+        Console.WriteLine(i);
+        Console.ReadLine();
+    }
+}
+*/
+
+/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte b = 256; //compile time error, byte max size  is 0 to 255
+        Console.WriteLine(b);
+        Console.ReadLine();
+    }
+}
+*/
+
+/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        short s = 256;
+        
+        Console.WriteLine(s);
+        Console.ReadLine();
+    }
+}
+*/
+
+/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        short s = 32768;//compile time error, short max value 32767
+        Console.WriteLine(s);
+        Console.ReadLine();
+    }
+}*/
+/*
+using System;  
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int b = 2147483648;//compile time error, int max value 2147483647
+        Console.WriteLine(b);
+        Console.ReadLine();
+    }
+}*/
+
+
+/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long b = 9223372036854775808l;//compile time error, long max value 9223372036854775807
+        Console.WriteLine(b);
+        Console.ReadLine();
+    }
+}*/
+/*
+using System;
+
+class Employe
+{
+    static void Main(string[] args)
+    {
+        //byte short int long deciml-->0-9
+        //bool--->true or false
+        //String or string "vani", "sachin", "vidisha","123","A","a","@#$%^&*"
+        //charecter char 'v''a''s''f''@''#' //it always come with singel qoutaion
+        //floating 200.59, 100.50, 200.67 float, float f=100.63f
+
+        string name = null;
+        string fname = null;
+        Employe emp = null;
+        Test tq = null;
+
+        Console.WriteLine(name);
+        Console.WriteLine(fname);
+        Console.WriteLine(emp);
+        Console.WriteLine(tq);
+        Console.ReadLine();
+    }
+}
+public class Test
+{
+
+}*/
+
